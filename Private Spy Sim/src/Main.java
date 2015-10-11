250a5b13a91b224e561f392132ce4a37262342bd
willle3456 <willle3456@gmail.com>
2015-10-11 11:45:08 -0700

    added project for game and main file for game

---
+++ Private Spy Sim/src/Main.java
@@ -0,0 +1,21 @@
+import processing.core.*;
+public class Main extends PApplet{
+	
+	//initialize everything in here
+	public void setup()
+	{
+			
+	}
+	
+	//where the logic goes
+	//loops as fast as it can
+	public void draw()
+	{
+		
+	}
+	public static void main(String [] args)
+	{
+		PApplet.main("Private Spy Sim");
+	}
+
+}
