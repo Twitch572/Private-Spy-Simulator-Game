484b0c03011a5cada754c6886d16fba861972d93
Kole <kjyager@calpoly.edu>
2015-10-24 14:46:48 -0700

    Investigated further render setup and made sure libraries work.

--- Private Spy Sim/src/Main.java
+++ Private Spy Sim/src/Main.java
@@ -1,6 +1,7 @@
 
 import processing.core.*;
 import core.*;
+import processing.opengl.*;
 
 public class Main extends PApplet{
 	
@@ -10,6 +11,7 @@ public class Main extends PApplet{
 	//Assets-----------
 	PImage frame;
 	PFont pixfont;
+	PShader MenuBG;
 	//-------------
 	
 	public static void main(String[] args) {
@@ -18,23 +20,34 @@ public class Main extends PApplet{
 
 	//initialize everything in here
 	public void setup() {
-		background(0);
+		background(90);
 		player = new PlayerStatus();
 		keyHandler = new KeyHandler(player);
 		frame = loadImage("../assets/ui/folderframe.png");
 		pixfont = createFont("../assets/ui/VCR_OSD_MONO.ttf",28);
+		MenuBG = loadShader("../assets/shaders/MenuFrag.glsl","../assets/shaders/MenuVert.glsl");
+	
 	}
 	public void settings() {
-		  size(GameSettings.WIN_DIMENSIONS[0], GameSettings.WIN_DIMENSIONS[1],P2D);
+		  size(GameSettings.WIN_DIMENSIONS[0], GameSettings.WIN_DIMENSIONS[1],P3D);
 	}
 	
 	//where the logic goes
 	//loops at 60 fps
 	public void draw() {
+		fill(0);
 		image(frame,(GameSettings.WIN_DIMENSIONS[0]-frame.width)/2,0);
 		textFont(pixfont);
 		text("The Game",236,68);
-		fill(0);
+		shader(MenuBG);
+		stroke(30);
+		/*fill(180);
+		beginShape();
+		vertex(0,0,-1);
+		vertex(GameSettings.WIN_DIMENSIONS[0],0,-9);
+		vertex(GameSettings.WIN_DIMENSIONS[0],GameSettings.WIN_DIMENSIONS[1],-9);
+		vertex(0,GameSettings.WIN_DIMENSIONS[1],-9);
+		endShape(CLOSE);*/
 	}
 	
 	public void keyPressed() {
