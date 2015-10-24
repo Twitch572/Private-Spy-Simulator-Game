26626d121b0f3e5fb898764648d7f1528064de32
Twitch572 <dude.dodiecat.dudet@gmail.com>
2015-10-24 11:51:44 -0700

    Background/Syntax/Test

--- Private Spy Sim/src/Main.java
+++ Private Spy Sim/src/Main.java
@@ -2,25 +2,21 @@ import processing.core.*;
 
 public class Main extends PApplet{
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		PApplet.main("Main");
 	}
 
 	//initialize everything in here
-	public void setup()
-	{
-			
+	public void setup() {
+		background(0);
 	}
 	public void settings() {
 		  size(200, 200);
 	}
 	
 	//where the logic goes
-	//loops as fast as it can
-	public void draw()
-	{
-
+	//loops at 60 fps
+	public void draw() {
 	    stroke(255);
 	    if (mousePressed) {
 	      line(mouseX,mouseY,pmouseX,pmouseY);
